464baa133b36fc7c2e27ab117c1e8b8094823ee5
nicolas rolland <nrolland@users.noreply.github.com>
2023-12-10 10:58:53 +0100

    script added

--- lakefile.lean
+++ lakefile.lean
@@ -10,3 +10,19 @@ lean_lib «Leftpad» where
 @[default_target]
 lean_exe «leftpad» where
   root := `Main
+
+
+/--
+Display a greeting
+
+USAGE:
+  lake run greet [name]
+
+Greet the entity with the given name. Otherwise, greet the whole world.
+-/
+script greet (args) do
+  if h : 0 < args.length then
+    IO.println s!"Hello, {args[0]'h}!"
+  else
+    IO.println "Hello, world!"
+  return 0
